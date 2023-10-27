9e6be1169e38f26bd04f4c4650b57fb24b608193
Teroaz <neoxdelta@gmail.com>
2023-10-27 16:50:58 +0200

    Systems Tests Coverage

---
+++ Shard.IntegrationTests/Systems/SystemsRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Shard.IntegrationTests.Systems;
+
+using Shard.Shared.Core;
+using Shard.Web.ImplementationAPI.Systems;
+
+public class SystemsRepositoryTests
+{
+    private readonly MapGenerator _mapGenerator;
+    private readonly SystemsRepository _repo;
+    private const string TestSeed = "testSeed";
+
+    public SystemsRepositoryTests()
+    {
+        var options = new MapGeneratorOptions { Seed = TestSeed };
+        _mapGenerator = new MapGenerator(options);
+        _repo = new SystemsRepository(_mapGenerator);
+    }
+
+    [Fact]
+    public void GetAllSystems_ReturnsAllSystems()
+    {
+        var systems = _repo.GetAllSystems();
+
+        Assert.Equal(10, systems.Count());
+    }
+
+    [Fact]
+    public void GetSystem_ReturnsCorrectSystem_WhenSystemNameIsValid()
+    {
+        var expectedSystemName = _mapGenerator.Generate().Systems[0].Name;
+
+        var system = _repo.GetSystem(expectedSystemName);
+
+        Assert.NotNull(system);
+        Assert.Equal(expectedSystemName, system.Name);
+    }
+
+    [Fact]
+    public void GetSystem_ReturnsNull_WhenSystemNameIsInvalid()
+    {
+        const string invalidSystemName = "InvalidSystemName";
+
+        var system = _repo.GetSystem(invalidSystemName);
+
+        Assert.Null(system);
+    }
+}
---
+++ Shard.IntegrationTests/Systems/SystemsServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Moq;
+using Shard.Shared.Core;
+using Shard.Web.ImplementationAPI.Models;
+using Shard.Web.ImplementationAPI.Systems;
+
+namespace Shard.IntegrationTests.Systems;
+
+public class SystemsServiceTests
+{
+    private readonly MapGenerator _mapGenerator;
+    private readonly Mock<ISystemsRepository> _mockRepo;
+    private readonly SystemsService _service;
+    private const string TestSeed = "testSeed";
+
+    public SystemsServiceTests()
+    {
+        var mapGeneratorOptions = new MapGeneratorOptions { Seed = TestSeed };
+        _mapGenerator = new MapGenerator(mapGeneratorOptions);
+        var sectorSpecification = _mapGenerator.Generate();
+
+        _mockRepo = new Mock<ISystemsRepository>();
+        _mockRepo.Setup(r => r.GetAllSystems()).Returns(sectorSpecification.Systems.Select(s => new SystemModel(s)));
+        _mockRepo.Setup(r => r.GetSystem(It.IsAny<string>())).Returns((string name) =>
+            sectorSpecification.Systems.Where(s => s.Name == name).Select(s => new SystemModel(s)).FirstOrDefault());
+
+        _service = new SystemsService(_mockRepo.Object);
+    }
+
+    [Fact]
+    public void GetAllSystems_ReturnsAllSystemsFromRepository()
+    {
+        var systems = _service.GetAllSystems();
+
+        Assert.Equal(_mapGenerator.Generate().Systems.Count, systems.Count);
+    }
+
+    [Fact]
+    public void GetSystem_ReturnsCorrectSystem_WhenSystemNameIsValid()
+    {
+        var targetSystemName = _mapGenerator.Generate().Systems[0].Name;
+
+        var system = _service.GetSystem(targetSystemName);
+
+        Assert.NotNull(system);
+        Assert.Equal(targetSystemName, system.Name);
+    }
+
+    [Fact]
+    public void GetSystem_ReturnsNull_WhenSystemNameIsInvalid()
+    {
+        const string invalidSystemName = "InvalidSystemName";
+        _mockRepo.Setup(r => r.GetSystem(invalidSystemName)).Returns((SystemModel?)null);
+
+        var system = _service.GetSystem(invalidSystemName);
+
+        Assert.Null(system);
+    }
+ 
+
+    [Fact]
+    public void GetRandomSystem_ReturnsNull_WhenNoSystemsAreAvailable()
+    {
+        _mockRepo.Setup(r => r.GetAllSystems()).Returns(new List<SystemModel>());
+
+        var randomSystem = _service.GetRandomSystem();
+        Assert.Null(randomSystem);
+    }
+    
+}
